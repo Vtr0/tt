154ca5519a8527034fc0500bb3f953b888b6ed41
Vtr0 <icwt@rocketmail.com>
2025-09-11 20:42:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 const _DEFAULT_CFG = {xState: {"history":{"currHis":0,"hisList":[]}},
-xDBFilesCfg: {"len":1,"toLoad":null}};
+xDBFilesCfg: {"len":2,"toLoad":null}};
 const ssData = {
 	wildCartStr: "<*~~*>",
 	xBookGrp: [{"grpWay":"Nhóm theo Bộ truyện",grp:[]},{"grpWay":"Sắp xếp theo Người đọc",grp:[]},{"grpWay":"Sắp xếp theo Tên truyện",grp:[]}]
+
 }
