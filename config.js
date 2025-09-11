6fd88c23e6f66caf10061799b0b300a730b87afd
Vtr0 <icwt@rocketmail.com>
2025-09-11 15:17:58 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+const _DEFAULT_CFG = {xState: {"history":{"currHis":0,"hisList":[]}},
+xDBFilesCfg: {"len":3,"toLoad":null}};
+const ssData = {
+	wildCartStr: "<*~~*>",
+	xBookGrp: [{"grpWay":"Nhóm theo Bộ truyện",grp:[]},{"grpWay":"Sắp xếp theo Người đọc",grp:[]},{"grpWay":"Sắp xếp theo Tên truyện",grp:[]}]
+}
